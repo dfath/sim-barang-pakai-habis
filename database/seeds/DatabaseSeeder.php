3dd04cd20391431a9cbdb1f7d5184b39b2a284d7
dfath <fathrico@gmail.com>
2019-08-12 07:27:38 +0700

    add UsersTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(InstansiTableSeeder::class);
         $this->call(UnitKerjaTableSeeder::class);
         $this->call(SatuanTableSeeder::class);
